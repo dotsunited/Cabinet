73c08a5e3fc8fe95006758748298cc6936ca0bd7
jsor <jan@sorgalla.com>
2011-03-16 15:07:40 +0100

    Adjust test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,40 +9,26 @@
  * file that was distributed with this source code.
  */
 
-// Get base and application path
-$rootPath = dirname(__DIR__);
-
-set_include_path(implode(PATH_SEPARATOR, array(
-    $rootPath . '/tests',
-    $rootPath . '/src',
-    get_include_path()
-)));
-
-// Setup autoloading
+// Setup PHPUnit autoloading from include_path
 spl_autoload_register(function($className) {
-    if (strpos($className, 'PHPUnit_') === false && strpos($className, 'DotsUnited\\') === false) {
-        return;
+    if (strpos($className, 'PHPUnit_') === 0) {
+        require str_replace('_', '/', $className) . '.php';
     }
+});
 
-    if (false !== strripos($className, '\\')) {
-        $replace = '\\';
-    } else {
-        $replace = '_';
+// Setup Cabinet test asset autoloading
+spl_autoload_register(function($className) {
+    if (strpos($className, 'DotsUnited\\Cabinet\\') === 0) {
+        $file = __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
+
+        if (file_exists($file)) {
+            require $file;
+        }
     }
+});
 
-    require str_replace($replace, DIRECTORY_SEPARATOR, $className) . '.php';
-}, true, true);
-
-// Define filters for clover report
-PHP_CodeCoverage_Filter::getInstance()->addDirectoryToWhitelist($rootPath . '/src');
-
-PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist($rootPath . '/tests');
-
-if (defined('PEAR_INSTALL_DIR') && is_dir(PEAR_INSTALL_DIR)) {
-    PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(PEAR_INSTALL_DIR);
+if (file_exists($file = __DIR__.'/../autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
+    require_once $file;
 }
-if (defined('PHP_LIBDIR') && is_dir(PEAR_INSTALL_DIR)) {
-    PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(PHP_LIBDIR);
-}
-
-unset($rootPath);
